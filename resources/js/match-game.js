c98ca3dfe86c07cea93b0d4698a51dc2356d2681
dbubic <dbubic@rogers.com>
2018-03-15 15:55:50 -0400

    modification of generateCards function

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -9,28 +9,28 @@ var MatchGame = {};
   Generates and returns an array of matching card values.
  */
 
-MatchGame.generateCardValues = function () {
-  var temp_array = [  ];
-  var n = 1;
-       for (var i = 0;  i = 7;  i++)  {
-         temp_array.push(n);
-         temp_array.push(n);
-    }
-    temp_array.splice(0, 1);
-    console.log(temp_array);
+ MatchGame.generateCardValues = function () {
+   var temp_array = [  ];
+   var n = 1;
+        for (var i = 0;  i < 8;  i++)  {
+          temp_array.push(n);
+          temp_array.push(n);
+          n++;
+     }
+     console.log(temp_array);
 
-    /*var count = 1;
-    var start_array = [ ];
-    while (count <= temp_array.length) {
-     function random_item(temp_array)  {
-       var x = temp_array[Math.floor(Math.random()*temp_array.length)];
-       start_array.push(x);
-       temp_array.splice(x, 1);
-       count=count+1;
-     };
-   }
-    console.log(start_array);*/
-  };
+   var count = 1;
+   var start_array = [ ];
+     while (count <= temp_array.length) {
+      function random_item(temp_array)  {
+        var x = temp_array[Math.floor(Math.random()*temp_array.length)];
+        start_array.push(x);
+        temp_array.splice(x, 1);
+        count++;
+      };
+    }
+     console.log(start_array);
+   };
 
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
